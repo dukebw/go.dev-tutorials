edcdc7fbf8e533d936f3c1289ecf63821158dda1
Brendan Duke <brendanw.duke@gmail.com>
2022-07-27 21:21:34 -0400

    Emit and handle errors in greetings/hello

--- create-module/hello/hello.go
+++ create-module/hello/hello.go
@@ -2,10 +2,22 @@ package main
 
 import (
   "fmt"
+  "log"
+
   "github.com/dukebw/go.dev-tutorials/create-module/greetings"
 )
 
 func main() {
-  message := greetings.Hello("Gladys")
+  // Set properties of the predefined Logger, including the log entry prefix
+  // and a flag to disable printing the time, source file, and line number.
+  log.SetPrefix("greetings: ")
+  log.SetFlags(0)
+
+  // Request a greeting message.
+  message, err := greetings.Hello("Gladys")
+  if err != nil {
+    log.Fatal(err)
+  }
+
   fmt.Println(message)
 }
