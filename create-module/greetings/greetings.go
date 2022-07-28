cc1497cc2a03c213a4e242c2b2f04fe8c2845420
Brendan Duke <brendanw.duke@gmail.com>
2022-07-27 21:59:37 -0400

    Emit random greeting from slice

--- create-module/greetings/greetings.go
+++ create-module/greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
   "errors"
   "fmt"
+  "math/rand"
+  "time"
 )
 
 // Hello returns a greeting for the named person.
@@ -13,6 +15,22 @@ func Hello(name string) (string, error) {
   }
 
   // Return a greeting that embeds the name in a message.
-  message := fmt.Sprintf("Hi, %v. Welcome!", name)
+  message := fmt.Sprintf(randomFormat(), name)
   return message, nil
+}
+
+func init() {
+  rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages.
+// The returned message is selected at random.
+func randomFormat() string {
+  formats := []string{
+    "Hi, %v. Welcome!",
+    "Great to see you, %v!",
+    "Hail, %v! Well met!",
+  }
+
+  return formats[rand.Intn(len(formats))]
 }
