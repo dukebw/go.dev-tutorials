e13a68fd0410fac2ed60e5ee161e0ef10c6a6656
Brendan Duke <brendanw.duke@gmail.com>
2022-07-28 13:46:47 -0400

    Format with gofmt. Add Hello tests

--- create-module/greetings/greetings.go
+++ create-module/greetings/greetings.go
@@ -1,51 +1,51 @@
 package greetings
 
 import (
-  "errors"
-  "fmt"
-  "math/rand"
-  "time"
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-  // If no name was given, return an error with a message.
-  if name == "" {
-    return "", errors.New("empty name")
-  }
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 
-  // Return a greeting that embeds the name in a message.
-  message := fmt.Sprintf(randomFormat(), name)
-  return message, nil
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
 }
 
 func Hellos(names []string) (map[string]string, error) {
-  messages := make(map[string]string)
+	messages := make(map[string]string)
 
-  for _, name := range names {
-    message, err := Hello(name)
-    if err != nil {
-      return nil, err
-    }
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
 
-    messages[name] = message
-  }
+		messages[name] = message
+	}
 
-  return messages, nil
+	return messages, nil
 }
 
 func init() {
-  rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 }
 
 // randomFormat returns one of a set of greeting messages.
 // The returned message is selected at random.
 func randomFormat() string {
-  formats := []string{
-    "Hi, %v. Welcome!",
-    "Great to see you, %v!",
-    "Hail, %v! Well met!",
-  }
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
 
-  return formats[rand.Intn(len(formats))]
+	return formats[rand.Intn(len(formats))]
 }
--- create-module/hello/hello.go
+++ create-module/hello/hello.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-  "fmt"
-  "log"
+	"fmt"
+	"log"
 
-  "github.com/dukebw/go.dev-tutorials/create-module/greetings"
+	"github.com/dukebw/go.dev-tutorials/create-module/greetings"
 )
 
 func main() {
-  // Set properties of the predefined Logger, including the log entry prefix
-  // and a flag to disable printing the time, source file, and line number.
-  log.SetPrefix("greetings: ")
-  log.SetFlags(0)
+	// Set properties of the predefined Logger, including the log entry prefix
+	// and a flag to disable printing the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 
-  names := []string{"Gladys", "John", "Mary"}
+	names := []string{"Gladys", "John", "Mary"}
 
-  // Request a greeting message.
-  messages, err := greetings.Hellos(names)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// Request a greeting message.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  fmt.Println(messages)
+	fmt.Println(messages)
 }
